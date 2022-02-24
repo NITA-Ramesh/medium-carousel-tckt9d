ede9e6661e4c83b854b33c87182709515dd50f6a
Ramesh Chand Yadav <nita.rameshyadav@gmail.com>
2022-02-24 17:49:39 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let slidePosition = 0;
 const slides = document.getElementsByClassName('carousel__item');
 const totalSlides = slides.length;
 
-document
-  .getElementById('carousel__button--next')
-  .addEventListener('click', function () {
+document.
+  getElementById('carousel__button--next')
+  .addEventListener("click", function() {
     moveToNextSlide();
   });
-document
-  .getElementById('carousel__button--prev')
-  .addEventListener('click', function () {
+document.
+  getElementById('carousel__button--prev')
+  .addEventListener("click", function() {
     moveToPrevSlide();
   });
 
@@ -18,28 +18,27 @@ function updateSlidePosition() {
     slide.classList.remove('carousel__item--visible');
     slide.classList.add('carousel__item--hidden');
   }
-
   slides[slidePosition].classList.add('carousel__item--visible');
 }
 
 function moveToNextSlide() {
-  if (slidePosition === 0) {
-    document.getElementById('carousel__button--prev').disabled = false;
+  if(slidePosition === 0){
+    document.getElementById("carousel__button--prev").disabled = false;
   }
-  if (slidePosition === totalSlides - 2) {
-    document.getElementById('carousel__button--next').disabled = true;
-  }
+  if (slidePosition === totalSlides-2) {
+    document.getElementById("carousel__button--next").disabled = true;
+  } 
   slidePosition++;
   updateSlidePosition();
 }
 
 function moveToPrevSlide() {
-  if (slidePosition === totalSlides - 1) {
-    document.getElementById('carousel__button--next').disabled = false;
+  if(slidePosition === totalSlides-1){
+    document.getElementById("carousel__button--next").disabled = false;
   }
   if (slidePosition === 1) {
-    document.getElementById('carousel__button--prev').disabled = true;
-  }
+    document.getElementById("carousel__button--prev").disabled = true;
+  } 
   slidePosition--;
   updateSlidePosition();
 }
